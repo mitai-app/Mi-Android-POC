0ef7cb3708e30cd4ec066b78111d6bc85c4cf21f
Charlton Smith <csmith12@amfam.com>
2022-01-06 04:36:24 -0500

    fixed up bin uploader

--- app/src/main/java/nyc/vonley/mi/di/network/impl/PS4ClientService.kt
+++ app/src/main/java/nyc/vonley/mi/di/network/impl/PS4ClientService.kt
@@ -4,11 +4,11 @@ import android.util.Log
 import nyc.vonley.mi.base.BaseClient
 import nyc.vonley.mi.di.modules.GuestInterceptorOkHttpClient
 import nyc.vonley.mi.di.network.ClientSync
+import nyc.vonley.mi.models.enums.Features
 import okhttp3.*
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
-import java.io.IOException
 import javax.inject.Inject
 
 class PS4ClientService @Inject constructor(
@@ -26,20 +26,28 @@ class PS4ClientService @Inject constructor(
      */
     fun uploadBin(file: ByteArray, callback: Callback) {
         if (target == null) return
-        val url = "http://$ip:9090"
-        val body: RequestBody = file.toRequestBody()
-        Log.e("URL", url)
-        post(url, body, Headers.headersOf(), callback)
+        val feature = target!!.features
+        feature.filter { it == Features.GOLDENHEN || it == Features.ORBISAPI }.flatMap {
+            return@flatMap it.ports.iterator().asSequence()
+        }.map { "$ip:$it" }.onEach { url ->
+            val body: RequestBody = file.toRequestBody()
+            Log.e("URL", url)
+            post(url, body, Headers.headersOf(), callback)
+        }
     }
 
     /**
      * Uploads file to web
      */
     fun uploadBin(file: File, callback: Callback) {
-        if (target == null) return
-        val body: RequestBody = file.asRequestBody()
-        val url = "$ip:9090"
-        post(url, body, Headers.headersOf(), callback)
+        val feature = target!!.features
+        feature.filter { it == Features.GOLDENHEN || it == Features.ORBISAPI }.flatMap {
+            return@flatMap it.ports.iterator().asSequence()
+        }.map { "$ip:$it" }.onEach { url ->
+            val body: RequestBody = file.asRequestBody()
+            Log.e("URL", url)
+            post(url, body, Headers.headersOf(), callback)
+        }
     }
 
 
